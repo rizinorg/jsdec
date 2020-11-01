ce9921622db2b35b56bb4945a10704517400971f
Florian Märkl <info@florianmaerkl.de>
2020-11-01 14:24:26 +0100

    Some CI update

--- .ci-scripts/ci-tests.sh
+++ .ci-scripts/ci-tests.sh
@@ -10,15 +10,15 @@ if [ "$ERRORED" == "1" ]; then
 	exit $ERRORED
 fi
 
-## r2dec-regression
+## jstest-test
 cd ..
 CI_WORKDIR=$(pwd)
 echo "Work dir: $CI_WORKDIR"
 
-rm -rf r2dec-regression >/dev/null 2>&1 || echo "no need to clean.."
-git clone --branch "$CI_BRANCH" --depth 1 https://github.com/radareorg/r2dec-regression || git clone --depth 1 https://github.com/radareorg/r2dec-regression
-cd r2dec-regression
+#rm -rf jstest-test >/dev/null 2>&1 || echo "no need to clean.."
+#git clone --branch "$CI_BRANCH" --depth 1 https://github.com/rizinorg/jstest-test || git clone --depth 1 https://github.com/rizinorg/jstest-test
+cd jstest-test
 chmod +x testall.sh
-./testall.sh "$CI_WORKDIR/r2dec-js" travis
+./testall.sh "$CI_WORKDIR/jsdec" travis
 ERRORED=$?
 cd ..
