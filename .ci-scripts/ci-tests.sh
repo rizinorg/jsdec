1de95ddc6ad05655e76f049a34a7d972bed21b74
wargio <wargio@libero.it>
2021-01-04 15:04:00 +0100

    cloning repo in the right place

--- .ci-scripts/ci-tests.sh
+++ .ci-scripts/ci-tests.sh
@@ -14,9 +14,10 @@ fi
 cd ..
 CI_WORKDIR=$(pwd)
 echo "Work dir: $CI_WORKDIR"
-find ../ -type d 
 
-cd ../jsdec-test
+rm -rf jsdec-test >/dev/null 2>&1 || echo "no need to clean.."
+git clone --branch "$CI_BRANCH" --depth 1 https://github.com/rizinorg/jsdec-test || git clone --depth 1 https://github.com/rizinorg/jsdec-test
+cd jsdec-test
 chmod +x testall.sh
 ./testall.sh "$CI_WORKDIR/jsdec" travis
 ERRORED=$?
