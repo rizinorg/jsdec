71cb78b1505aabcbf845653bb59aebc4d87a6b49
wargio <wargio@libero.it>
2021-01-04 14:55:46 +0100

    repo is in another castle

--- .ci-scripts/ci-tests.sh
+++ .ci-scripts/ci-tests.sh
@@ -14,11 +14,8 @@ fi
 cd ..
 CI_WORKDIR=$(pwd)
 echo "Work dir: $CI_WORKDIR"
-ls -lah
 
-#rm -rf jsdec-test >/dev/null 2>&1 || echo "no need to clean.."
-#git clone --branch "$CI_BRANCH" --depth 1 https://github.com/rizinorg/jsdec-test || git clone --depth 1 https://github.com/rizinorg/jsdec-test
-cd jsdec-test
+cd ../jsdec-test
 chmod +x testall.sh
 ./testall.sh "$CI_WORKDIR/jsdec" travis
 ERRORED=$?
