2ca930ae204d07365221812981cfc289f01b5325
wargio <wargio@libero.it>
2021-01-04 14:52:28 +0100

    trying to fix the ci to test the regressions on jsdec

--- .ci-scripts/ci-tests.sh
+++ .ci-scripts/ci-tests.sh
@@ -10,13 +10,14 @@ if [ "$ERRORED" == "1" ]; then
 	exit $ERRORED
 fi
 
-## jstest-test
+## jsdec-test
 cd ..
 CI_WORKDIR=$(pwd)
 echo "Work dir: $CI_WORKDIR"
+ls -lah
 
-#rm -rf jstest-test >/dev/null 2>&1 || echo "no need to clean.."
-#git clone --branch "$CI_BRANCH" --depth 1 https://github.com/rizinorg/jstest-test || git clone --depth 1 https://github.com/rizinorg/jstest-test
+#rm -rf jsdec-test >/dev/null 2>&1 || echo "no need to clean.."
+#git clone --branch "$CI_BRANCH" --depth 1 https://github.com/rizinorg/jsdec-test || git clone --depth 1 https://github.com/rizinorg/jsdec-test
 cd jsdec-test
 chmod +x testall.sh
 ./testall.sh "$CI_WORKDIR/jsdec" travis
